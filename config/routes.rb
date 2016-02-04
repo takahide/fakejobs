f0b0bd69c534324ed2fd9867bf9766a9cb679fd8
takahide <h@takahi.de>
2016-02-04 17:22:31 +0900

    something

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :jobs
   resources :tickets
   get '/' => 'top#index'
-  get '/t/:number' => 'top#ticket'
+#  get '/t/:number' => 'top#ticket'
   get '/job-careers/:number' => 'top#ticket'
   get '/d/:id' => 'top#job'
-  get '/job-details/:id' => 'top#job'
+#  get '/job-details/:id' => 'top#job'
 end
